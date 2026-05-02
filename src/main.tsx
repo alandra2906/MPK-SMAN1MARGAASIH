b46597343c2162b7f79da2787e7e7881039f88ce
Alandra <laptopgaya@gmail.com>
2026-05-02 21:05:09 +0700

    Enable Vercel Analytics and Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,23 @@ import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
 
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+
+// Di dalam fungsi render utama kamu:
+function MainApp() {
+  return (
+    <>
+      <App /> 
+      {/* Tambahkan dua baris ini di paling bawah sebelum penutup tag fragment */}
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
+}
+
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <App />
+    <MainApp />
   </BrowserRouter>,
 )
